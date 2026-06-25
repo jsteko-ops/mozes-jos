ec2f83b8f4c89726856275aa91eaf2bf9118d6ca
Josip <tvojemail@gmail.com>
2026-06-25 18:34:02 +0200

    fix auth page export

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,1 +1,50 @@
-<h1>TEST VERCEL UPDATE 123</h1>
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../lib/firebase";
+
+export default function AuthPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  async function login() {
+    setError("");
+
+    try {
+      await signInWithEmailAndPassword(auth, email.trim(), password);
+      router.push("/");
+    } catch (err: any) {
+      console.log(err.code);
+      setError(err.code);
+    }
+  }
+
+  return (
+    <main style={{ padding: "30px", maxWidth: "400px" }}>
+      <h1>Login 💪</h1>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Lozinka"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={login}>Login</button>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </main>
+  );
+}
