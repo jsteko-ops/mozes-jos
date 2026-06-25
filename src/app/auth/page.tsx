689f32240f3bd6f8d4b5d4c8d2d384ac9593695e
Josip <tvojemail@gmail.com>
2026-06-25 18:43:28 +0200

    fix auth page minimal

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,50 +1,9 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../lib/firebase";
-
-export default function AuthPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  const router = useRouter();
-
-  async function login() {
-    setError("");
-
-    try {
-      await signInWithEmailAndPassword(auth, email.trim(), password);
-      router.push("/");
-    } catch (err: any) {
-      console.log(err.code);
-      setError(err.code);
-    }
-  }
-
+export default function Page() {
   return (
-    <main style={{ padding: "30px", maxWidth: "400px" }}>
-      <h1>Login 💪</h1>
-
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      <input
-        type="password"
-        placeholder="Lozinka"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button onClick={login}>Login</button>
-
-      {error && <p style={{ color: "red" }}>{error}</p>}
+    <main style={{ padding: 30 }}>
+      <h1>LOGIN PAGE</h1>
     </main>
   );
 }
