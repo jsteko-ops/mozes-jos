6d008893ef0345dc0f1f633a2c455d7061390c38
Josip <tvojemail@gmail.com>
2026-06-25 18:45:26 +0200

    restore firebase login

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,48 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../lib/firebase";
+
 export default function Page() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  async function login() {
+    setError("");
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      router.push("/");
+    } catch (err: any) {
+      setError(err.code);
+    }
+  }
+
   return (
-    <main style={{ padding: 30 }}>
-      <h1>LOGIN PAGE</h1>
+    <main style={{ padding: 30, maxWidth: 400 }}>
+      <h1>Login 💪</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="Lozinka"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={login}>Login</button>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </main>
   );
 }
